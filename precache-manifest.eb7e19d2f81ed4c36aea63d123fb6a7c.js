459b515fb5be6d029c0640c398c2d6642c0a00a1
Rebeka Meszaros <akeber.m@live.com>
2021-02-18 16:58:10 +0000

    Updates

---
+++ precache-manifest.eb7e19d2f81ed4c36aea63d123fb6a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158a6140ef754c9e6366e59844657891",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "cdcda49db0f144cd4663",
+    "url": "/task-manager/static/css/main.87562bd7.chunk.css"
+  },
+  {
+    "revision": "4a1f9ef42788000d2e59",
+    "url": "/task-manager/static/js/2.a7e8e95f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-manager/static/js/2.a7e8e95f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdcda49db0f144cd4663",
+    "url": "/task-manager/static/js/main.c2e4c756.chunk.js"
+  },
+  {
+    "revision": "6f879587e75cb206c02c",
+    "url": "/task-manager/static/js/runtime-main.97270499.js"
+  }
+]);
